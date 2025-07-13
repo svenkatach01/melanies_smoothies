cbd9f7527c7bde2ced0735a99a3ee3d35e2bc5b3
svenkatach01 <subramani.venkatachalam@molex.com>
2025-07-13 13:43:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.write(
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothifroot_response.json(), use_container_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 name_on_order = st.text_input('Name for the order')
 st.write('The name on your Smoothie will be :', name_on_order)
