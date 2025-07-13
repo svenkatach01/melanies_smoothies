c3dfbfaf1a4e1c223d4261a53d38e4885332a6ee
svenkatach01 <subramani.venkatachalam@molex.com>
2025-07-13 12:41:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+
 from snowflake.snowpark.functions import col
-session = get_active_session()
+
+
+#session = get_active_session()
+cnx = st.connection('snowflake')
+session = cns.session()
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
 st.write(
