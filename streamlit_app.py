52633d1780cc8850ed512d941f2aa9ce1b4d1854
svenkatach01 <subramani.venkatachalam@molex.com>
2025-07-13 13:51:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.write(
   """
 )
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+
 #st.text(smoothiefroot_response.json())
 
 
@@ -38,6 +38,7 @@ if ingredients_list:
     ingredients_string=''
     for fruit_selected in ingredients_list:
         ingredients_string += fruit_selected + ' '
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_selected}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     st.write(ingredients_string)
     my_insert_string = """
